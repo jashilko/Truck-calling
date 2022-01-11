b1f44c9967177e3c59e4749bf7e01109d15aa31a
jashilko-hp <jashilko@gmail.com>
2022-01-12 00:54:54 +0300

    Доработки формата колл-файлов

--- TruckScript/CreateCallFile.py
+++ TruckScript/CreateCallFile.py
@@ -20,16 +20,16 @@ if not os.path.isdir(dir_store):
 def check_status():
     start_date = conf.get_setting(path, 'CreateCallFiles', 'Start')
     status = conf.get_setting(path, 'CreateCallFiles', 'Status')
-    if status == "Success":
-        return "Success"
-    elif status == "Off":
+    if status == "Off" and start_date != str(date.today()):
         return "Off"
     elif status == "Working":
-        if start_date == date.today():
+        if start_date == str(date.today()):
             return "Working"
         else:
             conf.update_setting(path, 'CreateCallFiles', 'Status', "Off")
             return "Off"
+    else:
+        return ""
 
 
 file_phones= os.path.join(dir_source, 'todo.json')
@@ -77,31 +77,52 @@ def create_call_files():
         try:
             one_file_name = os.path.join(dir_store, one_file_name)
             with open(one_file_name, "w") as file:
-                file.write("Channel: local/{}\n".format(value['0']))
-                file.write("MaxRetries: 2\n")
-                file.write("RetryTime: 60\n")
-                file.write("WaitTime: 30\n")
-                file.write("Context: from-internal\n")
-                file.write("Extension: 10\n")
-                file.write("Priority: 1")
+                if str(value['1']) == "": # Без контекста
+                    file.write("Channel: local/{}@avtoobzvon\n".format(value['0']))
+                    file.write("CallerID:\n")
+                    file.write("MaxRetries: 1\n")
+                    file.write("RetryTime: 60\n")
+                    file.write("WaitTime: 300\n")
+                    file.write("Context: avtoobzvon\n")
+                    file.write("Extension: play\n")
+                    file.write("Priority: 1")
+                else:
+                    file.write("Channel: local/{}@avtoobzvon-ivr\n".format(value['0']))
+                    file.write("CallerID: 6122103\n")
+                    file.write("MaxRetries: 0\n")
+                    file.write("RetryTime: 60\n")
+                    file.write("WaitTime: 300\n")
+                    file.write("Setvar: dtmf={}\n".format(value['1']))
+                    file.write("Setvar: CALLEE={}\n".format(value['0']))
+                    file.write("Context: avtoobzvon-ivr\n")
+                    file.write("Extension: play\n")
+                    file.write("Priority: 1")
         except Exception as e:
             print("Ошибка при сохранении файлов в store: %s" % str(e))
+
 
 def move_call_files():
     conf.update_setting(path, 'CreateCallFiles', 'Status', "Working")
     bundle = int(conf.get_setting(path, 'CreateCallFiles', 'Bundle'))
     timeout = int(conf.get_setting(path, 'CreateCallFiles', 'Timeout'))
     dir_target = conf.get_setting(path, 'Dirs', 'dir_target')
-    if not os.path.isdir(dir_target):
-        os.mkdir(dir_target)
-    count_file = 0
-    for file in os.listdir(dir_store):
-        if os.path.isfile(os.path.join(dir_store, file)):
-            move(os.path.join(dir_store, file), os.path.join(dir_target, file))
-            count_file += 1
-            if count_file == bundle:
-                time.sleep(timeout)
-                count_file = 0
+    try:
+        if not os.path.isdir(dir_target):
+            os.mkdir(dir_target)
+        count_file = 0
+        for file in os.listdir(dir_store):
+            if os.path.isfile(os.path.join(dir_store, file)):
+                move(os.path.join(dir_store, file), os.path.join(dir_target, file))
+                count_file += 1
+                if count_file == bundle:
+                    time.sleep(timeout)
+                    count_file = 0
+    except Exception as e:
+        print("Ошибка при перемещении файлов в target: %s" % str(e))
+    finally:
+        conf.update_setting(path, 'CreateCallFiles', 'Status', "Off")
+        conf.update_setting(path, 'CreateCallFiles', 'Start', str(date.today()))
+
 
 if __name__ == "__main__":
     if check_status() == "Off":
